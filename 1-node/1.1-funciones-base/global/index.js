a59574bc50148931a0e312bd6d7de3a255b3bb39
Gabriela Salazar <asmgabriela@gmail.com>
2022-07-05 21:23:06 -0500

    feat: Agrega comando del modulo global

---
+++ 1-node/1.1-funciones-base/global/index.js
@@ -0,0 +1,6 @@
+console.log("Ruta donde se encuentra mi archivo: ", __dirname);
+console.log("Ruta + nombre de mi archivo: ", __filename);
+
+// exports: Para exportar módulos (archivos)
+// module: Referencia al módulo actual
+// require(): Importa un archivo js como módulo. require("index.js")
