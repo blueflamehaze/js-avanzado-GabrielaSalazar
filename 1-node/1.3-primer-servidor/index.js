4d3aa21d346c0e46141bb9678d65146033f74d46
Gabriela Salazar <asmgabriela@gmail.com>
2022-07-06 21:25:25 -0500

    Mi primer servidor en node

---
+++ 1-node/1.3-primer-servidor/index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+// Status 200 => Ok
+// Status 304 => Not Modified - Ya respondí y no hubo un cambio
+// mimeType => Tipos de datos primarios que se pueden intercambiar entre servidor o un cliente. Es un objeto.
+
+const server = http.createServer((request, response) => {
+  const status = 200;
+  const mimeType = { "Content-Type": "text/html" };
+  // Escribir el head de la respuesta, recibe el status y el tipo de datos primarios que se van a intercambiar.
+  response.writeHead(status, mimeType);
+  // Escribir el archivo html
+  response.write("<html><body>Hola</body></html>");
+});
+
+server.listen(8080);
+console.log("Inicie el servidor");
